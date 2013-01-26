ebc9da359da8ce3b6cd73d919131cbdd78ef5f90
siem <siem@ubuntu.(none)>
2013-01-27 00:40:49 +0100

    tabs, adding and removing maps

--- second/main.js
+++ second/main.js
@@ -1,147 +1,72 @@
-$(function() {
-    $( "#draggable" ).draggable({stack:".ui-widget-content", cancel:"#map"}).resizable({stop: function(){google.maps.event.trigger(map, "resize");}, alsoResize: "#map"});
+var MapControl = function() {
+	this.map_count = 0;
+	Proj4js.defs["EPSG:28992"] = "+title=Amersfoort / RD New +proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs";
 
-    $( "#draggable2" ).draggable({stack:".ui-widget-content", cancel:"#map2" }).resizable({stop: function(){google.maps.event.trigger(map2, "resize");}, alsoResize: "#map2"});
-    
-    $("#draggable3" ).draggable({stack:".ui-widget-content", cancel:"#map3" }).resizable({stop: function(){map3.updateSize();}, alsoResize: "#map3"});
-    
-    $("#draggable4" ).draggable({stack:".ui-widget-content", cancel:"#map4" }).resizable({stop: function(){map4.updateSize();}, alsoResize: "#map4"});
+    this.loadDefaults = function() {
+        this.addMap("http://ec2-50-16-181-24.compute-1.amazonaws.com:8080/geoserver/wms", "ecotopen:Ecotopen", "100px", "10px");
+        this.addMap("http://mapsrv.ubvu.vu.nl/proxy/pub/service?", "Nederland 17e eeuw (Blaeu)_blaeu", "100px", "470px");
+        this.addMap("http://mapsrv.ubvu.vu.nl/proxy/pub/service?", "Nederland 17e eeuw (Blaeu)_blaeu", "100px", "900px");
 
-	$( "#tabs" ).tabs();
+    }
 	
-    $("#draggable5" ).draggable({stack:".ui-widget-content", cancel: "container"}); //.resizable({stop: function(){map4.updateSize();}, alsoResize: "#container"});
-    $("#draggable5").draggable( {cancel: "#contaimer"});
+	this.addMap = function(wms_url, layer_name, t, left) {	
+		this.map_count++;
+		var map_id = this.map_count;
+		
+        var map_container = $('<div id="draggable'+map_id+'"class="container ui-widget-content ">').appendTo("#tabs-2");
+        map_container.append('<p class="ui-widget-header">'+layer_name+' <button id="close'+map_id+'">Close</button></p>');
+        map_container.append('<div id="map'+map_id+'" class="map"></div></div>');
+		map_container.draggable({stack:".ui-widget-content", cancel:"#map"+map_id }).resizable({stop: function(){map.updateSize();}, alsoResize: "#map"+map_id});
+        //map_container.css("width","450px");
 
-    var lat_lng = new google.maps.LatLng(51.879193,5.772886);
-    var options = {
-        zoom: 14,
-        center: lat_lng,
-        mapTypeId: google.maps.MapTypeId.TERRAIN,
-        disableDoubleClickZoom: true
-    };
-    
-    var map = new google.maps.Map(document.getElementById("map"), options);
+        console.log(wms_url);
+        console.log(layer_name);
+        console.log(t);
+        console.log(left);
 
-//    console.log(options);
-
-    options.mapTypeId = google.maps.MapTypeId.SATELLITE;
-
-    var map2 = new google.maps.Map(document.getElementById("map2"), options);
-    
-    var map3 = new OpenLayers.Map('map3', {
-      maxResolution: 1226.5625,
-      maxExtent: new OpenLayers.Bounds(10000.0, 305000.0, 280000.0, 619000.0),
-      projection: new OpenLayers.Projection("EPSG:28992")
-    });
-    
-        
-    var historic = new OpenLayers.Layer.WMS(
-        "OpenLayers WMS",
-        "http://mapsrv.ubvu.vu.nl/proxy/pub/service?",
-        {layers:"Nederland 17e eeuw (Blaeu)_blaeu", format: "image/png", srs:"EPSG:28992"}, {singleTile:false, ratio: 1});
-    map3.addLayer(historic);
-    map3.zoomToMaxExtent();
-    
-    var map4 = new OpenLayers.Map('map4', {
-      maxResolution: 1226.5625,
-      maxExtent: new OpenLayers.Bounds(10000.0, 305000.0, 280000.0, 619000.0),
-      projection: new OpenLayers.Projection("EPSG:28992")
-    });
-    
-    var wms = new OpenLayers.Layer.WMS(
-        "OpenLayers WMS",
-        "http://mapsrv.ubvu.vu.nl/proxy/pub/service?",
-        {layers:"Top 25 raster (2009)_top_raster", format: "image/png", srs:"EPSG:28992"}, {singleTile:false, ratio: 1});
-
-    var wet = new OpenLayers.Layer.WMS(
-        "OpenLayers WMS wet",
-        "http://geodata.nationaalgeoregister.nl/digitaaltopografischbestand/ows?",
-        {layers:"digitaaltopografischbestand:vlakken", transparent: true, format: "image/png", srs:"EPSG:28992"}, {singleTile:false, ratio: 1}, {isBaseLayer: false} );
-
-
-    Proj4js.defs["EPSG:28992"] = "+title=Amersfoort / RD New +proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs";
-
-    var center = new OpenLayers.LonLat(new Array(5.772886, 51.879193));
-    var transformed = center.clone().transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection("EPSG:28992"));
-
-
-//    map4.addLayer(wms);
-    map4.addLayers([wms, wet]);
-    map4.addControl(new OpenLayers.Control.LayerSwitcher());
-    map4.setCenter(transformed, 8);
-//    map4.zoomToMaxExtent();
-
-
-    //CHARTS
-    Highcharts.setOptions({
-        global: {
-            useUTC: false
+        if ((top !== undefined) && (left !== undefined)) {
+            map_container.css("top", t);
+            map_container.css("left", left);
+            map_container.css("position","absolute");
         }
+        
+        $("#close"+map_id).button().click(function(event) {
+           var test = $(this).parent().parent();
+           $(test).remove();
+        });
+	
+
+		var map = new OpenLayers.Map('map'+map_id, {
+		  maxResolution: 1226.5625,
+		  maxExtent: new OpenLayers.Bounds(10000.0, 305000.0, 280000.0, 619000.0),
+		  projection: new OpenLayers.Projection("EPSG:28992")
+		});
+		
+		var layer = new OpenLayers.Layer.WMS(
+			"OpenLayers WMS",
+			wms_url,
+			{layers:layer_name, format: "image/png", srs:"EPSG:28992"}, {singleTile:false, ratio: 1});
+		map.addLayer(layer);
+		map.zoomToMaxExtent();
+	}
+}
+
+$(document).ready(function() {
+    var map_control = new MapControl();
+
+    $("#tabs").tabs({activate: function(event, ui) {map_control.loadDefaults()}});
+	
+   // map_control.loadDefault();
+
+	$("#add_map").button().click(function(event) {
+		map_control.addMap("http://mapsrv.ubvu.vu.nl/proxy/pub/service?", "Nederland 17e eeuw (Blaeu)_blaeu");
+		//$("#dialog-modal").dialog("open");
     });
-
-    var chart;
-    chart = new Highcharts.Chart({
-        chart: {
-            renderTo: 'container',
-            type: 'spline',
-            marginRight: 10,
-            events: {
-                load: function() {
-
-                    // set up the updating of the chart each second
-                    var series = this.series[0];
-                    setInterval(function() {
-                        var x = (new Date()).getTime(), // current time
-                            y = Math.random();
-                        series.addPoint([x, y], true, true);
-                    }, 1000);
-                }
-            }
-        },
-        xAxis: {
-            type: 'datetime',
-            tickPixelInterval: 150
-        },
-        yAxis: {
-            title: {
-                text: 'Value'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-        tooltip: {
-            formatter: function() {
-                    return '<b>'+ this.series.name +'</b><br/>'+
-                    Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
-                    Highcharts.numberFormat(this.y, 2);
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        exporting: {
-            enabled: false
-        },
-        series: [{
-            name: 'Random data',
-            data: (function() {
-                // generate an array of random data
-                var data = [],
-                    time = (new Date()).getTime(),
-                    i;
-
-                for (i = -19; i <= 0; i++) {
-                    data.push({
-                        x: time + i * 1000,
-                        y: Math.random()
-                    });
-                }
-                return data;
-            })()
-        }]
-    });
-
+	
+/*    $("#dialog-modal").dialog({
+		autoOpen:false,
+		height: 300,
+		modal: true
+	});
+*/
 });
