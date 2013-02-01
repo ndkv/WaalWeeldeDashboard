ad55973ac6740395df3c6a746b8fde854b47ec6a
Simeon Nedkov <simeon@ndkv.nl>
2013-02-01 20:17:00 +0000

    included getFeatureInfo: click on map to get data

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 var MapControl = function() {
+    //holds map objects
     this.maps = [];
+    //unique map ids
     this.map_count = 0;
+    //holds objects for which pan/zoom scroll is turned on
     this.lockList = [];
 
+
     Proj4js.defs["EPSG:28992"] = "+title=Amersfoort / RD New +proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs";
+
     this.initialize = true;
-	
 	this.map_store = {
 		ecotopen2005: {
 			url:"http://ec2-23-22-59-21.compute-1.amazonaws.com:8080/geoserver/wms",
@@ -24,10 +28,11 @@ var MapControl = function() {
 			layer: "ecotopen:ecotopen_2005",
 			title: "Biomassa 2005",
 			style: "biomassa"
-		},
+		}
 	}
 
-    this.loadDefaults = function(){
+
+    this.initialize = function(){
         if (this.initialize) {
             var t = 140;
             this.addMap(this.map_store.ecotopen2005_ruwheid, t, 10);
@@ -46,86 +51,138 @@ var MapControl = function() {
 		styles = map_params.style;
 		layer_title = map_params.title;
 		layer_name = map_params.layer;
+		wms_url_proxied = 'http://localhost:8000/__ajaxproxy/' + wms_url
 		
-		
-        var map_container = $('<div id="draggable'+map_id+'"class="container ui-widget-content ">').appendTo("#tabs-2");
-        map_container.append('<p class="ui-widget-header">'+layer_title+' </p>');
-        var map_toolbar = $('<div id="map_toolbar"></div>').appendTo(map_container);
-        map_toolbar.append('<div class="legend" id="legend_button'+map_id+'">Legenda</div>');
-        map_toolbar.append('<div class="close-button"  id="close'+map_id+'">Close</div>')
-	map_toolbar.append('<input type="checkbox" id="lock'+ map_id +'" /><label for="lock'+ map_id + '">Lock</label>');
-	var content = $('<div class="content"></div>').appendTo(map_container);
-	content.append('<div class="legend" id="legend'+map_id+'"></div>');
-        content.append('<div id="map'+map_id+'" class="map"></div></div>');
-	map_container.draggable({stack:".ui-widget-content", cancel:"#map"+map_id }).resizable({stop: function(){map.updateSize();}, alsoResize: "#map"+map_id});
 
+		//construct widget window
+		//TODO: put in a separate function
+        var map_container = $('<div id="draggable'+map_id+'"class="container ui-widget-content ">').appendTo("#tabs-2");
+		map_container.draggable({stack:".ui-widget-content", cancel:"#map"+map_id }).resizable({stop: function(){map.updateSize();}, alsoResize: "#map"+map_id});
         map_container.css("top", t+"px");
         map_container.css("left", left+"px");
-       
+        
+		map_container.append('<p class="ui-widget-header">'+layer_title+' </p>');
+        var map_toolbar = $('<div id="map_toolbar"></div>').appendTo(map_container);
+        map_toolbar.append('<input type="checkbox" id="legend_button' + map_id + '" /><label for="legend_button' + map_id + '">Legenda</label>');
+		map_toolbar.append('<input type="checkbox" id="lock'+ map_id +'" /><label for="lock'+ map_id + '">Lock</label>');
+        map_toolbar.append('<div class="close-button"  id="close'+map_id+'">Close</div>')
+		var content = $('<div class="content"></div>').appendTo(map_container);
+		//TODO ------ to here
+	
+		var legend_pane = $('<div class="legend" id="legend'+map_id+'"></div>').appendTo(content);
+        var map_pane = $('<div id="map'+map_id+'" class="map"></div></div>').appendTo(content);
 
-	//map toolbar buttons
-	//close
+		//map toolbar buttons
+		//close
         $("#close"+map_id).button().click(function(event) {
            $(this).parent().parent().remove();
         });
 
-	//legend
+		//legend
         var that = this;
-        $("#legend_button"+map_id).click(function(event) {
+        $("#legend_button"+map_id).button().click(function(event) {
+	    
+            $('#legend'+ map_id +' img');
+
+	
             return function () {
-		//TODO: pass content object instead
+			//TODO: pass content object instead
                 that.addLegend(layer_title, styles, map_id);
             }()
         });
 
-	//lock
+		//lock
 	
 
-	//map and layers
-	var map = new OpenLayers.Map('map'+map_id, {
+		//map and layers
+		var map = new OpenLayers.Map('map'+map_id, {
 	    maxResolution: 1226.5625,
 	    maxExtent: new OpenLayers.Bounds(10000.0, 305000.0, 280000.0, 619000.0),
 	    projection: new OpenLayers.Projection("EPSG:28992")
-	});
+		});
 
-	map.events.register('moveend', map, function(e) {
-		return function() {
-		    that.centerMaps(e, map_id, that);
-		}()
-	});
+		//register map events
+		map.events.register('moveend', map, function(e) {
+			return function() {
+				that.centerMaps(e, map_id, that);
+			}()
+		});
 
-	//TODO: add remove functionality
+		//TODO: add remove functionality
         this.maps.push(map);
 
         $("#lock"+map_id).button().click(function(event) {
             return function() {
-		if (that.lockList.indexOf(map_id) === -1) {
-		    that.lockList.push(map_id);
-		    $('#lock'+map_id).attr('checked', true)
-		} else {
-		    that.lockList.splice(that.lockList.indexOf(map_id), 1);
-		    $('#lock'+map_id).attr('checked', false);
-		};	
+				if (that.lockList.indexOf(map_id) === -1) {
+					that.lockList.push(map_id);
+	//		    $('#lock'+map_id).attr('checked', true)
+				} else {
+				that.lockList.splice(that.lockList.indexOf(map_id), 1);
+	//		    $('#lock'+map_id).attr('checked', false);
+			};	
 		
-		console.log(that.lockList);
+			console.log(that.lockList);
 	    }();
         });
 
 	
-	var layer = new OpenLayers.Layer.WMS(
+		var layer = new OpenLayers.Layer.WMS(
 		"OpenLayers WMS",
-		wms_url,
-		{layers:layer_name, format: "image/png", srs:"EPSG:28992", styles:styles}, {singleTile:false, ratio: 1});
-	map.addLayer(layer);
-	map.zoomToMaxExtent();		
-
-51.870927,6.066243
+			wms_url_proxied,
+			{layers:layer_name, format: "image/png", srs:"EPSG:28992", styles:styles}, {singleTile:false, ratio: 1});
+		
+		map.addLayer(layer);
+		map.zoomToMaxExtent();
 
         var center = new OpenLayers.LonLat(new Array(6.066243, 51.870927));
         var transformed = center.clone().transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection("EPSG:28992"));
         map.setCenter(transformed, 6);
+	
+	
+	
+		var getFeatureControl = new OpenLayers.Control.WMSGetFeatureInfo({
+			url: wms_url_proxied,			// Your WMS server url here,
+			drillDown: false, // Or true if you want drill down (see the docs)
+			hover: false, // Or true if you want but bear in mind this could get chatty
+			layers: [layer],
+			queryVisible: true,
+			eventListeners: {
+				getfeatureinfo: function (event) {
+					console.log('found it');
+					console.log(event.text);
 
-	};
+                    map.addPopup(new OpenLayers.Popup.FramedCloud(
+                        "chicken", 
+                        map.getLonLatFromPixel(event.xy),
+                        null,
+                        event.text,
+                        null,
+                        true
+                    ));
+					
+					// Code here if you want to process the results
+				},
+				beforegetfeatureinfo: function(event) {
+					//console.log('what');
+				  // Code here to set the content of queryableMapLayers
+				  // The event object will contain xy of mouse click
+				},
+				nogetfeatureinfo: function(event) {
+					console.log('cant find it');
+				  // Code here if no queryable layers are found
+				}
+			  } 
+		});
+		
+		map.addControl(getFeatureControl);
+		getFeatureControl.activate();
+
+};
+
+    this.showInfo = function(event) {
+        console.log('test');
+
+    }
 
     this.addLegend = function(layer_name, style, map_id) {
 		
@@ -179,7 +236,7 @@ var MapControl = function() {
 $(document).ready(function() {
     var map_control = new MapControl();
 
-    $("#tabs").tabs({activate: function(event, ui) {map_control.loadDefaults()}});
+    $("#tabs").tabs({activate: function(event, ui) {map_control.initialize()}});
 	
 	//$("#add-map").button().click(function(event) {
 		//map_control.addMap("http://mapsrv.ubvu.vu.nl/proxy/pub/service?", "Nederland 17e eeuw (Blaeu)_blaeu", "Nederland 17de eeuw", 240, 120, "");
